1ab0e3cf748935460640707d313be0fa609d83eb
InfiniteX <infinitexyy@gmail.com>
2019-06-11 16:38:27 +0800

    update post

---
+++ blog/projects/today/precache-manifest.0ca2b94876db5d009c8999da6ebdfefa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "89b363a262b235a7b442",
+    "url": "./static/css/main.74b20690.chunk.css"
+  },
+  {
+    "revision": "89b363a262b235a7b442",
+    "url": "./static/js/main.bf9ed4e6.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "26e18d1068c9070cb1b3",
+    "url": "./static/css/2.0b17528d.chunk.css"
+  },
+  {
+    "revision": "26e18d1068c9070cb1b3",
+    "url": "./static/js/2.4109c323.chunk.js"
+  },
+  {
+    "revision": "0692bd599bc6bcc44f42944b887f530d",
+    "url": "./static/media/baidu.0692bd59.svg"
+  },
+  {
+    "revision": "d4d1faad4afd1e3a2278128f8baca767",
+    "url": "./static/media/google.d4d1faad.svg"
+  },
+  {
+    "revision": "645da7096a2134ea1b28d936e45111f4",
+    "url": "./index.html"
+  }
+];
