e47aeebd2e14f24ca5d12498f041c00efb709fd8
InfiniteXyy <InfiniteXyy@users.noreply.github.com>
2022-05-21 15:09:32 +0000

    deploy: fffe68062087e1a73ca7aba4d00a3b6c85bd53ba

---
+++ _next/static/Jy7fe4XTlZC5c58qt623g/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/379-b0502b28c07a4e97.js","static/chunks/pages/index-8cc160568f5b3c4c.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/ZZtYY3KSwR0qayFazbj2G/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/379-b0502b28c07a4e97.js","static/chunks/pages/index-5f33b1d36924a5fb.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
